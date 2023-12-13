84de756b12a5f9488e18873f82cddff2e47b7caa
zaratemari <153635697+zaratemari@users.noreply.github.com>
2023-12-13 03:57:18 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './navigation';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Banner } from './components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          holaaa
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Footer />
+      <Header />
+      <Banner />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// import Comp1 from './Componente/Comp1';
+// import Footer from './components/Footer';
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//       <Footer />
+//       <Comp1 />
+//       sjgf
+//     </div>
+//   );
+// }
+
+// export default App;
